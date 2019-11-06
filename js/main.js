821833642cbb30137850f910884b755563d1656b
Bohdan <bomtvv@gmail.com>
2019-11-07 01:46:34 +0200

    add icons and create circle button

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
-var rippleBtns = getEl('.ripple')
-
-//creating a ripple effect for button
-function spawnRipple(width, height, posX, posY, rippleBtn){
-	calcWidth = ( width <= height ) ? height : width
-	calcHeight = ( width <= height ) ? height : width
-	calcTop = posY - (calcHeight * 0.5)
-	calcLeft = posX - (calcWidth * 0.5)
-
-	var rippleEl = document.createElement('span')
-	rippleEl.classList.add("btn-ripple")
-	rippleEl.style.width = calcWidth + "px"
-	rippleEl.style.height = calcHeight + "px"
-	rippleEl.style.top = calcTop + "px"
-	rippleEl.style.left = calcLeft + "px"
-	rippleBtn.appendChild(rippleEl)
-}
+var rippleBtns = document.querySelectorAll('.ripple')
 
 rippleBtns.forEach(function(rippleBtn){
 
