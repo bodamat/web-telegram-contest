41bcc09a464e1796e7b5b789a220a2943efc9d09
Bohdan <bomtvv@gmail.com>
2019-11-12 01:32:45 +0200

    left sidebar settings

--- alpha/js/main.js
+++ alpha/js/main.js
@@ -3,13 +3,16 @@ var rippleBtns = document.querySelectorAll('.ripple')
 rippleBtns.forEach(function(rippleBtn){
 
 	rippleBtn.addEventListener('mousedown', function(e){
+
 		//get the button's width and height
 		var width = this.offsetWidth
 		var height = this.offsetHeight
 		
 		//get the cursor's x and y position within the button
-		var posX = e.pageX - this.offsetLeft
-		var posY = e.pageY - this.offsetTop
+		// var posX = e.pageX - this.offsetLeft
+		// var posY = e.pageY - this.offsetTop
+		var posX = e.offsetX
+		var posY = e.offsetY
 		
 		spawnRipple(width, height, posX, posY, this)
 	});
